d3fb5807483c2aab9805f5a9db37b1396f9c267e
Ulasovets Vladimir <ulasovetsvladimir@gmail.com>
2024-05-11 22:54:36 +0300

    fixed outputs.tf file

--- 02/src/outputs.tf
+++ 02/src/outputs.tf
@@ -1,31 +1,9 @@
-###web instance
-output "web_instance_name" {
-  value       = yandex_compute_instance.platform.name
-  description = "web instance name"
+output "web_instance" {
+  value       = [yandex_compute_instance.platform.name, yandex_compute_instance.platform.network_interface[0].nat_ip_address, yandex_compute_instance.platform.fqdn]
+  description = "web instance instance_name, external_ip, fqdn"
 }
 
-output "web_external_ip" {
-  value       = yandex_compute_instance.platform.network_interface
-  description = "web instance network interface"
-}
-
-output "web_fqdn" {
-  value       = yandex_compute_instance.platform.fqdn
-  description = "web instance network interface"
-}
-
-###db instance
-output "db_instance_name" {
-  value       = yandex_compute_instance.platform-db.name
-  description = "db instance name"
-}
-
-output "db_external_ip" {
-  value       = yandex_compute_instance.platform-db.network_interface
-  description = "db instance network interface"
-}
-
-output "db_fqdn" {
-  value       = yandex_compute_instance.platform-db.fqdn
-  description = "db instance network interface"
+output "db_instance" {
+  value       = [yandex_compute_instance.platform-db.name, yandex_compute_instance.platform-db.network_interface[0].nat_ip_address, yandex_compute_instance.platform-db.fqdn]
+  description = "db instance instance_name, external_ip, fqdn"
 }
