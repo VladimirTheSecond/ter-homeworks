d04649363f3c3fcd01dd804e30a1c10f948d161a
Ulasovets Vladimir <ulasovetsvladimir@gmail.com>
2024-05-14 23:06:35 +0300

    created count-vm.tf file

---
+++ 03/src/count-vm.tf
@@ -0,0 +1,31 @@
+resource "yandex_compute_instance" "web" {
+  for_each = toset([1,2])
+  name = "web-${each.key}"
+  platform_id = var.platform_id
+  zone        = var.default_zone
+  resources {
+    cores         = var.vms_resources.web.cores
+    memory        = var.vms_resources.web.memory
+    core_fraction = var.vms_resources.web.core_fraction
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu.image_id
+    }
+  }
+  scheduling_policy {
+    preemptible = true
+  }
+  network_interface {
+    subnet_id = yandex_vpc_subnet.develop.id
+    nat       = true
+  }
+
+  metadata = {
+    serial-port-enable = var.vms_metadata.ubuntu.serial-port-enable
+    ssh-keys           = var.vms_metadata.ubuntu.ssh-keys
+  }
+
+}
+}
--- 03/src/variables.tf
+++ 03/src/variables.tf
@@ -30,3 +30,8 @@ variable "vpc_name" {
   default     = "develop"
   description = "VPC network&subnet name"
 }
+
+variable "platform_id" {
+  type        = string
+  default     = "standard-v1"
+}
